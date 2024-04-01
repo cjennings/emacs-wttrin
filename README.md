84b79776b53e83bbe9563e51ef1099f42ef40183
Craig Jennings <craigmartinjennings@gmail.com>
2024-04-01 11:19:41 -0500

    customize font, display date, clean requeries, license change, &c.
    - changed license to GPLv3
    - allow user to specify font name and height for display
    - place date/time stamp above location name at top of buffer
    - re-query with new location deletes to reduce buffer clutter
    - updated screenshot to show current updates
    - changed references from "city" to "location" in wttrin.el
    - removed old CI config files
    - removed old badges from readme file

--- README.md
+++ README.md
@@ -1,52 +1,57 @@
-[![MELPA][melpa-badge]][melpa-package]
-[![MELPA Stable][melpa-stable-badge]][melpa-stable-package]
-[![Gitter][gitter-badge]][gitter-chatroom]
-[![Build Status][travis-ci-badge]][travis-ci-status]
-
 # wttrin.el
 
-Emacs frontend for weather web service [wttr.in].
+Emacs frontend for weather web service [wttr.in](https://wttr.in).
 
-This fork has the following patches applied (still outstanding in bcbcarl/emacs-wttrin): 
-- patch for selecting the unit system: https://github.com/bcbcarl/emacs-wttrin/pull/10
-- patch for url request to return ascii rather than html https://github.com/bcbcarl/emacs-wttrin/pull/18
-- patch to use https instead of http: https://github.com/bcbcarl/emacs-wttrin/pull/15
+This fork has a few minor improvements:
+- It displays the date and time	the weather was displayed.
+- It allows for customizing the font and font height used to display.
+- On location re-query, the original weather buffer is closed, reducing buffer clutter.
+- The license has changed from the MIT license to the GNU General Public License Version 3.
+
+In addition, this fork applies the following patches (outstanding in bcbcarl/emacs-wttrin):
+- [patch to selecting the unit system](https://github.com/bcbcarl/emacs-wttrin/pull/10)
+- [patch to request the return ascii instead of html](https://github.com/bcbcarl/emacs-wttrin/pull/18)
+- [patch to use https instead of http](https://github.com/bcbcarl/emacs-wttrin/pull/15)
+
+All changes have been tested and are working as of Monday, April 01, 2024.
+
+Bug reports and PRs are welcome.
+
+Enjoy!
 
 ## Usage
 
 Set a default cities list for completion:
 
 ```elisp
-(setq wttrin-default-cities '("Taipei" "Tainan"))
-```
+(setq wttrin-default-cities '( "Berkeley, California" "Taipei, Taiwan" "Tainan, Taiwan"))```
 
 You can also specify default HTTP request Header for Accept-Language:
 
 ```elisp
-(setq wttrin-default-accept-language '("Accept-Language" . "zh-TW"))
-```
+(setq wttrin-default-accept-language '("Accept-Language" . "zh-TW")))```
+
 As well as the unit system, "m" for metric and "u" for USCS/imperial (the default is to choose based on your location):
 
 ```elisp
-(setq wttrin-unit-system "m")
-```
-Then run `M-x wttrin` to get the information.
+(setq wttrin-unit-system "m")```
 
-When the weather is displayed you can press `q` to quit the buffer or `g` to query for another city.
+If you want the weather to display in a monospaced font other than your default, you may specify it:
 
-![screenshot]
+```elisp
+(setq wttrin-font-name "Hack Nerd Font Mono")```
+
+You can specify the font height as well:
+
+```elisp
+(setq wttrin-font-name "Liberation Mono")```
+
+Then run `M-x wttrin` to display the weather.
+
+After the weather is displayed you may press `q` to quit the buffer or `g` to query the weather for another location.
+
+![screenshot](https://raw.githubusercontent.com/cjennings/emacs-wttrin/master/wttrin.png?raw=true)
 
 ## LICENSE
 
-MIT
-
-[wttr.in]: http://wttr.in/
-[screenshot]: wttrin.png
-[melpa-badge]: http://melpa.org/packages/wttrin-badge.svg
-[melpa-package]: http://melpa.org/#/wttrin
-[melpa-stable-badge]: http://stable.melpa.org/packages/wttrin-badge.svg
-[melpa-stable-package]: http://stable.melpa.org/#/wttrin
-[gitter-badge]: https://badges.gitter.im/bcbcarl/emacs-wttrin.svg
-[gitter-chatroom]: https://gitter.im/bcbcarl/emacs-wttrin?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge
-[travis-ci-badge]: https://travis-ci.org/bcbcarl/emacs-wttrin.svg?branch=master
-[travis-ci-status]: https://travis-ci.org/bcbcarl/emacs-wttrin
+GPL-v3
--- wttrin.el
+++ wttrin.el
@@ -16,14 +16,39 @@
 ;;; Code:
 
 (require 'url)
-(require 'xterm-color)
+(require 'xterm-color) ;; https://github.com/atomontage/xterm-color
 
 (defgroup wttrin nil
   "Emacs frontend for weather web service wttr.in."
   :prefix "wttrin-"
   :group 'comm)
 
-(defcustom wttrin-default-cities '("Berkeley" "New Orleans" "New York" "London" "Taipei")
+(defcustom wttrin-font-name "Liberation Mono"
+  "Preferred monospaced font name for weather display."
+  :group 'wttrin
+  :type 'string)
+
+(defcustom wttrin-font-height 130
+  "Preferred font height for weather display."
+  :group 'wttrin
+  :type 'integer)
+
+(defface wttrin-buffer-face
+  `((t :height ,wttrin-font-height :family ,wttrin-font-name))
+  "Default face for the weather display buffer."
+  :group 'wttrin)
+
+(defcustom wttrin-default-cities '("Berkeley, California"
+                                   "New Orleans, Louisiana"
+                                   "New York, New York"
+                                   "London, England"
+                                   "Paris, France"
+                                   "Berlin, Germany"
+                                   "Naples, Italy"
+                                   "Athens, Greece"
+								   "Kyiv, Ukraine"
+								   "Tainan, Taiwan"
+                                   "Taipei, Taiwan")
   "Specify default cities list for quick completion."
   :group 'wttrin
   :type 'list)
@@ -43,47 +68,71 @@ Use 'm' for 'metric', 'u' for 'USCS, or nil for location based units (default)."
   "Concatenates extra information into the URL."
   (concat "?" wttrin-unit-system))
 
-(defun wttrin-fetch-raw-string (query) "Get the weather information based on your QUERY."
-	   (let ((url-user-agent "curl"))
-	 (add-to-list 'url-request-extra-headers wttrin-default-accept-language)
-	 (with-current-buffer
-		 (url-retrieve-synchronously
-		  (concat "http://wttr.in/" query "?A")
-		  (lambda (status) (switch-to-buffer (current-buffer))))
-	   (decode-coding-string (buffer-string) 'utf-8))))
+(defun wttrin-fetch-raw-string (query)
+  "Get the weather information based on your QUERY."
+  (let ((url-user-agent "curl"))
+    (add-to-list 'url-request-extra-headers wttrin-default-accept-language)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         (concat "http://wttr.in/" query "?A")
+         (lambda (status) (switch-to-buffer (current-buffer))))
+      (decode-coding-string (buffer-string) 'utf-8))))
 
 (defun wttrin-exit ()
   "Exit the wttrin buffer."
   (interactive)
   (quit-window t))
 
+(defun wttrin-restart ()
+  "Kill buffer and restart wttrin."
+  (interactive)
+  (kill-buffer)
+  (call-interactively 'wttrin))
+
 (defun wttrin-query (city-name)
   "Query weather of CITY-NAME via wttrin, and display the result in new buffer."
   (let ((raw-string (wttrin-fetch-raw-string city-name)))
     (if (string-match "ERROR" raw-string)
-		(message "Cannot retrieve weather data. Perhaps the city name was misspelled?")
+		(message "Cannot retrieve weather data. Perhaps the location was misspelled?")
       (let ((buffer (get-buffer-create (format "*wttr.in - %s*" city-name))))
         (switch-to-buffer buffer)
         (setq buffer-read-only nil)
         (erase-buffer)
-        (insert (xterm-color-filter raw-string))
+
+        ;; set the preferred font attributes for this buffer only
+        (setq buffer-face-mode-face `(:family  ,wttrin-font-name :height  ,wttrin-font-height))
+        (buffer-face-mode t)
+
+		;; insert weather data
+		(insert (xterm-color-filter raw-string))
+
+		;; remove all info lines except date and location
+		(goto-char (point-min))
+		(kill-whole-line 4)
+		(forward-line)
+		(kill-whole-line)
+
+		;; provide user instructions
+        (goto-char (point-max))
+        (insert "\n")
+        (insert "Press 'g' for another location's weather, 'q' to quit.")
+
+		;; align buffer to top
         (goto-char (point-min))
-        (re-search-forward "^$")
-		(delete-region (point-min) (1+ (point)))
-		(goto-char (point-max))
-		(insert "\n")
-		(insert "Press 'g' for another city's weather, 'q' to quit.")
+
+		;; create choice keymap and disallow modifying buffer
         (use-local-map (make-sparse-keymap))
         (local-set-key "q" 'wttrin-exit)
-		(local-set-key "g" 'wttrin)
-		(setq buffer-read-only t)))))
+		(local-set-key "g" 'wttrin-restart)
+
+        (setq buffer-read-only t)))))
 
 ;;;###autoload
 (defun wttrin (city)
   "Display weather information for CITY."
   (interactive
    (list
-	(completing-read "City Name: " wttrin-default-cities nil nil
+    (completing-read "City Name: " wttrin-default-cities nil nil
                      (when (= (length wttrin-default-cities) 1)
                        (car wttrin-default-cities)))))
   (wttrin-query city))
