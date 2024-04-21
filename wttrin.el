2e60c808a37e8b0b7a601479ad1b4785f2522a70
Craig Jennings <c@cjennings.net>
2024-04-21 13:25:17 -0500

    Add License Boilerplate and SPDX-License-Identifier for GPL 3.0

--- wttrin.el
+++ wttrin.el
@@ -10,6 +10,23 @@
 ;; Keywords: weather, wttrin, games
 ;; URL: https://github.com/cjennings/emacs-wttrin
 
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;; This file is NOT part of GNU Emacs.
+
 ;;; Commentary:
 ;; Displays the weather information from the wttr.in service for your submitted
 ;; location.
